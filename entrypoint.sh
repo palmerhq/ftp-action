4e8ae3a8e15baa28ebdde666e7f0005fa0cc3a30
Jared Palmer <jared@palmer.net>
2020-10-07 18:19:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "mirror -v -R -P --exclude .git/ --exclude .github/ --exclude node_modules/ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "mirror -v -R --exclude .git/ --exclude .github/ --exclude node_modules/ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
