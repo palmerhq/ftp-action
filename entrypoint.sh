4bccd3146341109569eee36d9408695fb2526dee
Jared Palmer <jared@palmer.net>
2020-10-08 12:21:03 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "mirror --parallel=3 --verbose -v -R $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror -v -R  --exclude .git/  --exclude .DS_Store --exclude .data/ --exclude node_modules/ --verbose=3 --parallel=8  $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
