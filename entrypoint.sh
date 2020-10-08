1f243d373e4557f3caf8ee53e2954b10da43370e
Jared Palmer <jared@palmer.net>
2020-10-08 12:14:10 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "mirror -v -R $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "mirror --parallel=3 --verbose -v -R $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
