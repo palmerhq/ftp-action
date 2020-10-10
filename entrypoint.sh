6a3e22ed3784bcf1a398cfd80b7c4271e017c6b0
Jared Palmer <jared@palmer.net>
2020-10-10 13:16:02 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror -v -R  --exclude .git/  --exclude .DS_Store --exclude .data/ --exclude node_modules/ --verbose=3 --parallel=8  $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp sftp://$INPUT_USER:$INPUT_PASSWORD@$INPUT_HOST -e "set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; set sftp:auto-confirm yes; mirror -v -R  --exclude .git/ --verbose=3 --parallel=8  $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
